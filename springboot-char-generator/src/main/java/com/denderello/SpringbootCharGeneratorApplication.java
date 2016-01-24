8f554c8c6c0b64f4f3f24164515917d13e189784
Dennis Benkert <spinecrasher@googlemail.com>
2016-01-24 18:03:43 +0100

    added consul support

--- springboot-char-generator/src/main/java/com/denderello/SpringbootCharGeneratorApplication.java
+++ springboot-char-generator/src/main/java/com/denderello/SpringbootCharGeneratorApplication.java
@@ -2,8 +2,10 @@ package com.denderello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringbootCharGeneratorApplication {
 
 	public static void main(String[] args) {
--- springboot-word-generator/src/main/java/com/denderello/domain/CharacterRepository.java
+++ springboot-word-generator/src/main/java/com/denderello/domain/CharacterRepository.java
@@ -1,16 +1,21 @@
 package com.denderello.domain;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 
 @Repository
 public class CharacterRepository {
 
+    @Autowired
+    @LoadBalanced
+    private RestTemplate restTemplate;
+
     @HystrixCommand(fallbackMethod = "getCharacterFallback")
     public Character getCharacter() {
-        RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject("http://character/character", Character.class);
+        return this.restTemplate.getForObject("http://character/character", Character.class);
     }
 
     public Character getCharacterFallback() {
